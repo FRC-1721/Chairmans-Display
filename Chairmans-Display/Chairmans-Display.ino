31a84a216fc376fb339cc5a744166bf7fa3960d1
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-10 20:25:00 -0400

    Move folder, add features

--- Chairmans-Display/Chairmans-Display.ino
+++ Chairmans-Display/Chairmans-Display.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 30 // Number of LEDs in the strip
+#define NUM_LEDS 600 // Number of LEDs in the strip
 #define DATA_PIN 7
 
 // Define the array of leds
@@ -12,11 +12,12 @@ void setup() {
 
 void loop() { 
   // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
   FastLED.show();
   delay(500);
   // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
+  //leds[0] = CRGB::Black;
+  FastLED.clear();
   FastLED.show();
   delay(500);
 }
